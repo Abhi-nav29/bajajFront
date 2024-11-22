555cba364e16f84c4de6318062a1e10b8ed14966
Abhi-nav29 <abhinavsisodiyaseoni@gmail.com>
2024-11-22 17:58:37 +0530

    new changes

--- src/components/JsonProcessor.js
+++ src/components/JsonProcessor.js
@@ -68,7 +68,7 @@ const JsonProcessor = () => {
       }
       formData.append('data', JSON.stringify(validJson.data));
 
-      const response = await fetch('https://bajajbackend-1jk6.onrender.com/bfhl', {
+      const response = await fetch('https://bajajback-pffg.onrender.com/bfhl', {
         method: 'POST',
         headers: {
           'user_id': 'test_user',
